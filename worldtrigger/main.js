e533f740128ae0f2ebb8b4649b6b57c5e18596ec
c3210272 <134463943+c3210272@users.noreply.github.com>
2023-10-17 11:16:25 +0900

    wtr

---
+++ worldtrigger/main.js
@@ -0,0 +1,6 @@
+window.onload = function () {
+    const loadingImage = document.getElementById('loadingImage');
+    loadingImage.classList.add('visible');
+    const spinner = document.getElementById('spinner');
+    spinner.style.display = 'none';
+}
